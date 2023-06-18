91cf969bfba299acf59b8484646d837159d26470
mikerena2077 <40911023+mikerena2077@users.noreply.github.com>
2023-06-18 14:56:19 -0700

    Add files via upload

---
+++ sleep.py
@@ -0,0 +1,12 @@
+
+#import time library
+import time
+ 
+# printing the start time
+print("The time of code execution begin is : ", time.ctime())
+ 
+# using sleep() to hault the code execution
+time.sleep(6)
+ 
+# printing the end time
+print("The time of code execution end is : ", time.ctime())
